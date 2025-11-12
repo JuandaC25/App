3c594d49a9270b570a528aa684b0a6c987b3874b
Juanda <cruzjuanda8a@gmail.com>
2025-11-12 17:24:32 -0500

    app model,dto,services and repository

---
+++ backend/src/main/com/app/backend/dto/LoginRequest.java
@@ -0,0 +1,21 @@
+package com.app.backend.dto;
+
+public class LoginRequest{
+    private String username;
+    private String password;
+
+    public LoginRequest(){
+    }
+
+    public String getUsername(){
+        reuturn username;
+    }
+
+    public String getPassword(){
+        reuturn password;
+    }
+
+    public void setPassword(String password){
+        this.password = password; 
+    }
+}
---
+++ backend/src/main/com/app/backend/dto/UserCreateRequest.java
@@ -0,0 +1,54 @@
+package com.app.backend.dto;
+
+import com.app.backend.model.User;
+
+public class UserCreateRequest{
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private String active;
+
+    public UserCreateRequest(){
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public void setRole(String role){
+        this.role = role;
+    }
+
+    public String getActive(){
+        return active;
+    }
+
+    public void setActive(String active){
+        this.active = active;
+    } 
+}
